f7c82924060831500db776b1e14841798b34af7a
K4RF <rpdirwk1025@naver.com>
2024-06-09 03:46:18 +0900

    명령어 권한 추가 명령어

--- Beemo_Mama/main.py
+++ Beemo_Mama/main.py
@@ -10,6 +10,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(current_dir, 'config.json')
 recent_numbers_path = os.path.join(current_dir, 'recent_numbers.json')
 music_list_path = os.path.join(current_dir, 'music_list.json')
+authorized_users_path = os.path.join(current_dir, 'authorized_users.json')
 
 # config.json 파일에서 설정 읽기
 try:
@@ -40,6 +41,14 @@ except Exception as e:
     print(f"Error reading music_list.json: {e}")
     music_list = []
 
+# authorized_users.json 파일에서 권한 있는 사용자 목록 읽기
+try:
+    with open(authorized_users_path, 'r', encoding='utf-8') as f:
+        authorized_users = set(json.load(f))
+except Exception as e:
+    print(f"Error reading authorized_users.json: {e}")
+    authorized_users = set()
+
 # 디스코드 인텐트 설정
 intents = discord.Intents.default()
 intents.message_content = True
@@ -52,10 +61,14 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
+# 사용자 권한 체크 함수
+def check_authorization(user_id):
+    return user_id == authorized_user_id or user_id in authorized_users
+
 # "!랜덤곡" 명령어 처리
 @bot.command(name='랜덤곡')
 async def random_music_command(ctx):
-    if ctx.author.id == authorized_user_id:
+    if check_authorization(ctx.author.id):
         try:
             recent_numbers.clear()
             if not music_list:
@@ -69,12 +82,12 @@ async def random_music_command(ctx):
         except Exception as e:
             await ctx.send(f'오류 발생: {e}')
     else:
-        await ctx.send('이 명령어를 사용할 권한이 없습니다.')
+        await ctx.send('님 권한 없음 ㅅㄱ.')
 
 # "!다음곡" 명령어 처리
 @bot.command(name='다음곡')
 async def next_music_command(ctx):
-    if ctx.author.id == authorized_user_id:
+    if check_authorization(ctx.author.id):
         try:
             if not music_list:
                 await ctx.send('음악 리스트가 비어 있습니다. music_list.json 파일을 확인하세요.')
@@ -94,12 +107,23 @@ async def next_music_command(ctx):
         except Exception as e:
             await ctx.send(f'오류 발생: {e}')
     else:
-        await ctx.send('님 권한 없음 ㅅㄱ')
+        await ctx.send('님 권한 없음 ㅅㄱ.')
 
 # "!경기시작" 명령어 처리
 @bot.command(name='경기시작')
 async def start_game_command(ctx):
     await ctx.send('경기 시자ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ악 하겠습니다!!!!!!!!!!!!!!')
+
+# "!권한추가" 명령어 처리
+@bot.command(name='권한추가')
+async def add_authorization_command(ctx, user: discord.User):
+    if ctx.author.id == authorized_user_id:
+        authorized_users.add(user.id)
+        with open(authorized_users_path, 'w', encoding='utf-8') as f:
+            json.dump(list(authorized_users), f, ensure_ascii=False)
+        await ctx.send(f'{user.name}님 명령어 쓰셈')
+    else:
+        await ctx.send('님 권한 없음 ㅅㄱ.')
 
 # 봇 실행
 try:
